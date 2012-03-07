16b606651ac374069f64acc03a85055fab9823bc
Kabir Kukreti <kabir@gnowledge.org>
2012-03-07 12:39:38 +0530

    Form fields have been reordered to make data entry easy

--- gstudio/forms.py
+++ gstudio/forms.py
@@ -4,48 +4,44 @@ from django.contrib.admin import widgets
 
 class MetatypeForm(ModelForm):
 
-    class Meta:
-        model = Metatype
+
+	class Meta:
+		model = Metatype
+		fields = ('title', 'altnames','plural','parent','slug','creation_date','last_update','description','parent')
+
 
 class ObjecttypeForm(ModelForm):                 
 
-
-
-
-
-
-    class Meta:
-        model = Objecttype
-        fields = ('title', 'altnames','plural','parent','slug','metatypes','tags',
-                      'status','content','prior_nodes','posterior_nodes','password','login_required','sites')
+	    class Meta:
+		    model = Objecttype
+		    fields = ('title', 'altnames','plural','parent','slug','metatypes','tags',
+			      'status','content','prior_nodes','posterior_nodes','password','login_required','sites')
 
 class AttributetypeForm(ModelForm):
 
-    class Meta:
-         model = Attributetype
-         fields =('title','altnames','subjecttype','applicable_nodetypes','dataType',
-		  'slug','status','content','prior_nodes','posterior_nodes','password','login_required','sites')
+	class Meta:
+		model = Attributetype
+		fields =('title','altnames','subjecttype','applicable_nodetypes','dataType',
+			 'slug','status','content','prior_nodes','posterior_nodes','password','login_required','sites')
 
 class RelationtypeForm(ModelForm):
 
-    class Meta:
-         model = Relationtype
-         fields =('title','altnames','slug','inverse','left_subjecttype','left_applicable_nodetypes','right_subjecttype',
-		 'right_applicable_nodetypes','content','prior_nodes','posterior_nodes','sites')
+	class Meta:
+		model = Relationtype
+		fields =('title','altnames','slug','inverse','left_subjecttype','left_applicable_nodetypes','right_subjecttype', 'right_applicable_nodetypes','content','prior_nodes','posterior_nodes','sites')
 class SystemtypeForm(ModelForm):
 
-    class Meta:
-         model =Systemtype
-         fields =('title','altnames','content','parent','slug','status','nodetype_set','relationtype_set','attributetype_set','metatype_set','processtype_set',
+	class Meta:
+		model =Systemtype
+		fields =('title','altnames','content','parent','slug','status','nodetype_set','relationtype_set','attributetype_set','metatype_set','processtype_set',
 		 'prior_nodes','posterior_nodes','sites')
 
 
 class ProcesstypeForm(ModelForm):
 
-    class Meta:
-         model =Processtype
-         fields =('title','altnames','content','parent','slug','status','changing_attributetype_set','changing_relationtype_set',
-		 'prior_nodes','posterior_nodes','sites')
+	class Meta:
+		model =Processtype
+		fields =('title','altnames','content','parent','slug','status','changing_attributetype_set','changing_relationtype_set','prior_nodes','posterior_nodes','sites')
 
 
 class RelationForm(ModelForm):
@@ -53,13 +49,13 @@ class RelationForm(ModelForm):
         model = Relation
 
 class AttributeForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(AttributeForm, self).__init__(*args, **kwargs)
-        self.fields['last_update'].widget = widgets.AdminSplitDateTime()
-        self.fields['creation_date'].widget = widgets.AdminSplitDateTime()
+	def __init__(self, *args, **kwargs):
+		super(AttributeForm, self).__init__(*args, **kwargs)
+		self.fields['last_update'].widget = widgets.AdminSplitDateTime()
+		self.fields['creation_date'].widget = widgets.AdminSplitDateTime()
 
-    class Meta:
-        model = Attribute
+	class Meta:
+		model = Attribute
 
 
 class ComplementForm(ModelForm):
